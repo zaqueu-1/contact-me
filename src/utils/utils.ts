b3ab304aec1938325a23079adaaa7774a24f4bd3
Eduardo Zaqueu <bss.eduardo@yahoo.com.br>
2025-02-11 14:49:36 -0300

    fix utils.ts import path and handleString func

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-import { FormValues } from "@/types"
+import { FormValues } from "@/app/types"
 
 export const handleAddInputs = (data: FormValues) => {
   if (!data.name || !data.tel) {
@@ -17,6 +17,17 @@ export const handleEditInputs = (data: FormValues) => {
 export const handleCreationDate = (date: string) => {
   const newDate = new Date(date)
   return newDate.toLocaleDateString()
+}
+
+export const handleString = (str: string) => {
+  if (str.length > 20) {
+    return str.substring(0, 20) + "..."
+  }
+  return str
+}
+
+export const handleTel = (tel: string) => {
+  return tel.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3")
 }
 
 export const errorMsg = (msg: string) => {
